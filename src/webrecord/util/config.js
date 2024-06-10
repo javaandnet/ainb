a902ef7292094db50c4ae8778240fd465ff6ff03
Ren <javaandnet@gmail.com>
2024-06-10 21:09:43 +0900

    config del

--- src/webrecord/util/config.js
+++ src/webrecord/util/config.js
@@ -3,7 +3,7 @@ module.exports = class {
     openai = {
         organization: "org-7JnZDaFZRVxEBIwVWj84ptjj",//
         project: "proj_OslquGFuzI1ln4zehBXhdCpI",
-        apiKey: "sk-proj-zn1jlb6Gh89rVh7jDcBBT3BlbkFJkNb5sttlaGqIWwNxss3R"
+        apiKey: "sk-proj-zn1jlb6Gh89rVh7jDcBBT3BlbkFJkNb5sttlaGqIWwNxss3Rww"
     };
     azure = {
         subscriptionKey: "6f78e68a9ef543988c4866e30d46bbae",
