fbe46643c8ccbd879f30aeb05e2dc6aa201b66b5
Ren <javaandnet@gmail.com>
2024-06-20 00:47:47 +0900

    公司插件 增加外置功能

--- src/server/assistant/company.js
+++ src/server/assistant/company.js
@@ -5,130 +5,145 @@ const mail = new Mail();
 const sf = new SF();
 const util = new Util();
 class Company {
+    DEBUG=false;
     id = "asst_KQsWjF05lR95Z92JwpOMCZBE";
     config = {
         name: "会社の営業",
         instructions: "あなたはFSR株式会社の営業です。会社、技術者、案件情報、面接の情報をお客さんに紹介する。メール送信前再確認必要です。",
         model: "gpt-3.5-turbo",
-        tools: [
-            {
-                type: "function",
-                function: {
-                    name: "getInfo",// 绑定到函数
-                    description: "会社情報を取得する",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            query: { description: "質問の範囲" },
-                            condition: { description: "質問の条件", type: "string" }
-                        },
-                        required: ["query", "condition"],//必须
+        tools: [{
+            type: "function",
+            function: {
+                name: "changePrice",// 绑定到函数
+                description: "修改提案单价",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        name: { description: "技術者の名前", type: "string" },
+                        price: { description: "修改单价的内容", type: "string" }
                     },
+                    required: ["name", "price"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "getEmp",// 绑定到函数
-                    description: "社員の情報を取得する、説明文、履歴書など、一覧の場合は番号を付けて出力してください",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            query: { description: "質問の範囲" },
-                            condition: { description: "質問の条件", type: "string" }
-                        },
-                        required: ["query", "condition"],//必须
+            },
+        },
+        {
+            type: "function",
+            function: {
+                name: "getInfo",// 绑定到函数
+                description: "会社情報を取得する、会社の名前、社長、社員数",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        query: { description: "質問の内容", type: "string" }
                     },
+                    required: ["query"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "getProject",// 绑定到函数
-                    description: "案件関連の情報,未完了案件、具体情報",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            no: { description: "案件の番号", type: "string" },
-                            condition: { description: "検索の条件", type: "string" },
-                        },
-                        required: ["condition"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "getEmp",// 绑定到函数
+                description: "社員の情報を取得する、説明文、履歴書など、一覧の場合は番号を付けて出力してください",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        no: { description: "社員の番号", type: "string" },
+                        query: { description: "質問の範囲", type: "string" },
+                        name: { description: "社員の名前、一般的なに漢字の場合", type: "string" }
                     },
+                    required: ["query", "no", "name"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "selectInfo",// 绑定到函数
-                    description: "案件または技術者を選択します。あるいは既存の選択したものから外す",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            type: { description: "選択のタイプ、案件か技術者か", type: "string" },
-                            flag: { description: "選択または外すフラグ、選択の場合は１とする、その以外は０とする", type: "string" },
-                            info: { description: "選択と外すの内容", type: "string" }
-                        },
-                        required: ["type", "info", "flag"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "getProject",// 绑定到函数
+                description: "案件関連の情報,未完了案件、具体情報",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        no: { description: "案件の番号", type: "string" },
+                        condition: { description: "検索の条件", type: "string" },
                     },
+                    required: ["condition"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "addInfo",// 绑定到函数
-                    description: "案件または技術者を追加する",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            type: { description: "選択のタイプ、案件か技術者か", type: "string" },
-                            name: { description: "案件名または技術者の名前", type: "string" }
-                        },
-                        required: ["type", "name"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "selectInfo",// 绑定到函数
+                description: "案件または技術者を選択します。あるいは既存の選択したものから外す",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        type: { description: "選択のタイプ、案件か技術者か", type: "string" },
+                        flag: { description: "選択または外すフラグ、選択の場合は１とする、その以外は０とする", type: "string" },
+                        info: { description: "選択と外すの内容", type: "string" }
                     },
+                    required: ["type", "info", "flag"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "changeStatus",// 绑定到函数
-                    description: "案件または技術者の状態を変更する。あるいは既存の選択したものから外す",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            type: { description: "選択のタイプ、案件か技術者か", type: "string" },
-                            flag: { description: "開始または停止フラグ、開始の場合は１とする、その以外は０とする", type: "string" },
-                            info: { description: "操作の内容、名前、番号など", type: "string" }
-                        },
-                        required: ["type", "info", "flag"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "addInfo",// 绑定到函数
+                description: "案件または技術者を追加する",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        type: { description: "選択のタイプ、案件か技術者か", type: "string" },
+                        name: { description: "案件名または技術者の名前", type: "string" }
                     },
+                    required: ["type", "name"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "sendMail",// 绑定到函数
-                    description: "お客さんにメールを送信する、その前に送信情報を再確認必要です。実際送信または確認フラグを「実際」と「確認」に分けてください。",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            mailto: { description: "送信先", type: "string" },
-                            isConfirm: { description: "実際送信または確認フラグ", type: "string" },
-                            emp: { description: "送信の内容と関連する技術者、誰の何場合は誰を設定する", type: "string" },
-                            info: { description: "発送内容の特定する、例えば、誰の何の場合は何を設定する", type: "string" }
-                        },
-                        required: ["mailto", "isConfirm"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "changeStatus",// 绑定到函数
+                description: "案件または技術者の状態を変更する。あるいは既存の選択したものから外す",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        type: { description: "選択のタイプ、案件か技術者か", type: "string" },
+                        flag: { description: "開始または停止フラグ、開始の場合は１とする、その以外は０とする", type: "string" },
+                        info: { description: "操作の内容、名前、番号など", type: "string" }
                     },
+                    required: ["type", "info", "flag"],//必须
                 },
-            }, {
-                type: "function",
-                function: {
-                    name: "confirmMail",// 绑定到函数
-                    description: "お客さんにメールを送信前確認して、必要な場合は送信する。",
-                    parameters: {
-                        type: "object",
-                        properties: {//参数说明
-                            mailto: { description: "送信先", type: "string" },
-                            isConfirm: { description: "実際送信または確認フラグ", type: "string" },
-                            info: { description: "発送内容", type: "string" }
-                        },
-                        required: ["query", "condition", "isConfirm"],//必须
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "sendMail",// 绑定到函数
+                description: "お客さんにメールを送信する、その前に送信情報を再確認必要です。実際送信または確認フラグを「実際」と「確認」に分けてください。",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        mailto: { description: "送信先", type: "string" },
+                        isConfirm: { description: "実際送信または確認フラグ", type: "string" },
+                        emp: { description: "送信の内容と関連する技術者、誰の何場合は誰を設定する", type: "string" },
+                        info: { description: "発送内容の特定する、例えば、誰の何の場合は何を設定する", type: "string" }
                     },
+                    required: ["mailto", "isConfirm"],//必须
                 },
-            }
+            },
+        }, {
+            type: "function",
+            function: {
+                name: "confirmMail",// 绑定到函数
+                description: "お客さんにメールを送信前確認して、必要な場合は送信する。",
+                parameters: {
+                    type: "object",
+                    properties: {//参数说明
+                        mailto: { description: "送信先", type: "string" },
+                        isConfirm: { description: "実際送信または確認フラグ", type: "string" },
+                        info: { description: "発送内容", type: "string" }
+                    },
+                    required: ["query", "condition", "isConfirm"],//必须
+                },
+            },
+        }
         ]
     };
     out = {
@@ -142,16 +157,25 @@ class Company {
             obj.option = args;
             return args;
         },
+
+        changePrice: async function (args, obj) {
+            obj.option = args;
+            return args;
+        },
+
         addInfo: async function (args, obj) {
             obj.option = args;
             var insertObj = { Name: args.name, Detail__c: args.KEYWORDSTR, status__c: 0 };
             var rtn = await sf.insert("Project__c", insertObj);
-            console.log(rtn);
-            console.log("addInfo out func:", rtn);
+            //console.log(rtn);
+            //console.log("addInfo out func:", rtn);
             return args;
         }
     };
     func = {
+        changePrice: async function (args) {
+            return "OK";
+        },
         doProjectWithWorker: async function (args) {
             return "情報がありません";
         },
@@ -165,6 +189,8 @@ class Company {
                 return "会社の名前はFSR株式会社です。"
             } else if (args.name == "inactive") {
                 return await sf.workerNoWork();
+            } else if (args.name == "emp") {
+                return "105人です";
             }
             return "情報がありません";
         },
@@ -222,15 +248,17 @@ class Company {
                 }
             } else if (util.defined(args.no)) {
                 var data = await sf.projectByNo(args.no);
-                const project_str = util.objToStr({ "案件号": data.no, "案件名": data.name, "内容": data.detail });
-                return {
-                    ai: JSON.stringify({ id: data.id, no: data.no }), //只有名字
-                    out: project_str
-                };
+                if (data != null) {
+                    const project_str = util.objToStr({ "案件号": data.no, "案件名": data.name, "内容": data.detail });
+                    return {
+                        ai: JSON.stringify({ id: data.id, no: data.no }), //只有名字
+                        out: project_str
+                    };
+                }
             }
-            else {
-                return { ai: "情報なし", out: "案件情報がありません" };
-            }
+
+            return { ai: "情報なし", out: "案件情報がありません" };
+
 
         },
 
@@ -239,11 +267,26 @@ class Company {
         },
 
         getEmp: async function (args) {
-
             if (args.status == "inactive") {
-                return await sf.workerNoWork();
-            } else {
-                var info = await sf.workByName(args.name);
+                // return await sf.workerNoWork();
+                // var sql = "SELECT Id,  Name,AutoNumber__c  FROM Worker__c  where SalesStatus__c = '可能'";
+                var data = await sf.find("Worker__c", { SalesStatus__c: '可能' }, "Id, Name, Status__c, AutoNo__c", 50);
+                if (data == null || data.totalSize == 0) {
+                    return { ai: "情報なし", out: "技術者情報がありません" };
+                } else {
+                    var worker = {};
+                    var ai = {};
+                    data.forEach((element, index) => {
+                        worker[element.AutoNo__c] = element.Name;
+                    });
+                    return {
+                        ai: util.objToStr(worker), //只有名字
+                        out: util.objToStr(worker)
+                    };
+                }
+            } else if (args.name) {
+                var info = await sf.empByName(args.name);
+
                 if (info == null) {
                     return { ai: "情報なし", out: "技術者情報がありません" };
                 }
@@ -251,7 +294,12 @@ class Company {
                     ai: JSON.stringify({ id: info.id, emp: info.name }), //只有名字
                     out: info.information
                 };
+            } else if (args.no) {
+                var data = await sf.empByNo(args.no);
+                return { ai: "情報なし", out: "技術者情報がありません" };
             }
+            return { ai: "情報なし", out: "技術者情報がありません" };
+
         },
         confirmMail: async function (args) {
             var address = {
@@ -270,7 +318,7 @@ class Company {
             };
             var emp = {};
             if (args.emp) {
-                emp = await sf.workByName(args.emp);
+                emp = await sf.empByName(args.emp);
             } else {
                 return { ai: "送信内容を不明です", out: "どうの内容を不明です" };
             }
@@ -336,12 +384,11 @@ class Company {
                 , "ceo": "ceo"
                 , "president": "ceo"
                 , "company": "name"
+                , "会社名": "name"
+                , "社名": "name"
                 , "name": "name"
                 , "名前": "name"
-                , "未稼働": "inactive"
-                , "inactive": "inactive"
-                , "未稼働": "inactive"
-                , "稼働していない": "inactive"
+                , "社員数": "emp"
             });
             name = util.getNullStr(name, "");
             args.name = name;
@@ -351,8 +398,9 @@ class Company {
         getProject: async function (args) {
             var status = util.getArg(args, ["status"], {
                 "未完了": "0",
-                "一覧": "-1",
-                "all": "-1",
+                "一覧": "0",
+                "all": "0",
+                "全ての案件": "0",
                 "incomplete": "0"
                 , "complete": "9"
             }, "status:"
@@ -374,7 +422,6 @@ class Company {
             return args;
         },
         getEmp: async function (args) {
-
             var status = util.getArg(args, ["status", "employment_status"], {
                 "未稼働": "inactive",
                 "inactive": "inactive",
@@ -394,6 +441,16 @@ class Company {
             if (name != null) {
                 args.name = name;
             }
+            if ((name == null) && (args.query)) {
+                args.no = args.query;
+            }
+            if (args.name.indexOf("FSR") >= 0) {
+                args.no = args.name;
+            }
+            if( args.no.indexOf("FSR") >= 0){
+                delete args.name;
+            }
+            //FSR-TW654
             return args;
         }
     }
