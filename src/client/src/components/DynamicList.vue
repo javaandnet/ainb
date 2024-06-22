c309824f595382888d866522c5297f1f49d90751
Ren <javaandnet@gmail.com>
2024-06-23 08:29:42 +0900

    动态List组件

---
+++ src/client/src/components/DynamicList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <van-list v-model="list">
+      <div v-for="(item, index) in list" :key="index" class="list-item">
+        <van-icon :name="item.icon" />
+        <span class="span-left">{{ item.text }}</span>
+        <van-icon name="close" @click="removeItem(index)" />
+      </div>
+    </van-list>
+    <van-field v-model="newItem" placeholder="输入新项" clearable />
+    <van-button type="primary" @click="addItem">添加</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      newItem: "",
+    };
+  },
+  methods: {
+    setList(list) {
+      this.list = list;
+    },
+    getIcon(type) {
+      //企業
+      if (type == 0) {
+        return "";
+        //案件
+      } else if (type == 1) {
+        return "description-o";
+        //Mail
+      } else if (type == 2) {
+        return "envelop-o";
+        //個人
+      } else if (type == 3) {
+        return "user-o";
+        //その他
+      } else {
+        return "";
+      }
+    },
+
+    addItem() {
+      if (this.newItem.trim() !== "") {
+        this.list.push(this.newItem);
+        this.newItem = "";
+      }
+    },
+    removeItem(index) {
+      this.list.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+.span-left {
+  position: relative;
+  padding-left: 4px;
+  width: 100%;
+  left: 2px; /* 向右偏移20px */
+  text-align: left; /* 文本左对齐 */
+}
+
+.list-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: left;
+  padding: 10px;
+  border-bottom: 1px solid #ebebeb;
+}
+</style>
--- src/client/src/js/testData.js
+++ src/client/src/js/testData.js
@@ -1,10 +1,20 @@
 export default class TestData {
+
+    getSenderList = function () {
+        return [
+            { type: "0", text: "FSR", value: "11" },
+            { type: "1", text: "SAP案件", value: "11" },
+            { type: "2", text: "任 nin@fsr.co.jp", value: "11" },
+            { type: "3", text: "任", value: "11" },
+        ]
+    };
+
     listMsg = function () {
         return {
             mode: "list", model: "worker", list: [{
                 text: "高大芳",
                 value: "a05F300000HYu46IAD"//Id,   
-            },{
+            }, {
                 text: "王瀚達",
                 value: "a05F300000HYu68IAD"//Id,   
             }]
