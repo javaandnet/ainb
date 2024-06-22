00fb5a7c0320eb87f344020b59556557bfe10769
Ren <javaandnet@gmail.com>
2024-06-23 08:30:29 +0900

    动态List组件

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
+import { Icon } from 'vant';
 import App from './App.vue';
 import Vant from 'vant';
 import axios from 'axios';
 import 'vant/lib/index.css';
 // 配置Axios
 const app = createApp(App);
+app.use(Icon);
 // Add axios to global properties
 app.config.globalProperties.$axios = axios;
 app.use(Vant);
