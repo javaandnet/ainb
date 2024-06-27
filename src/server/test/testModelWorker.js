f3f3d87db40aae073f87c2df29579e3626c62300
Ren <javaandnet@gmail.com>
2024-06-28 00:18:04 +0900

    [feature]测试类例子

---
+++ src/server/test/testModelWorker.js
@@ -0,0 +1,22 @@
+import Worker from '../model/worker.js';
+import TestUtil from '../test/testUtil.js';
+
+const test = new TestUtil();
+
+const worker = new Worker();
+
+
+async function testFunc(type, isHtml) {
+    await test.testAwait(worker, "info", "a05F300000HYu5xIAD", "Id, Name, Status__c, AutoNo__c,Japanese__c,TecLevel__c, Information__c, NameToOuter__c,Resume__c", type, isHtml);
+}
+// async function testInfo(id, fields) {
+//     console.log(await worker.info(id, fields, 0, true));
+//     console.log(await worker.info(id, fields, 1, true));
+//     console.log(await worker.info(id, fields, 0, false));
+//     console.log(await worker.info(id, fields, 1, false));
+// }
+// testInfo("a05F300000HYu5xIAD", "Id, Name, Status__c, AutoNo__c,Japanese__c,TecLevel__c, Information__c, NameToOuter__c,Resume__c");
+await testFunc(0, true);
+await testFunc(1, true);
+await testFunc(0, false);
+await testFunc(1, false);
--- src/server/test/testutil.js
+++ src/server/test/testutil.js
@@ -1,93 +1,44 @@
-import { AI } from './util/ai.js';
-import Util from './util/util.js';
-const util = new Util();
-import { Config } from "./util/config.js";
-import OpenAI from "openai";
-import { Azure } from './util/azure.js';
-import fs from "fs";
-const azure = new Azure();
-const openai = new OpenAI(Config.openai);
-const ai = new AI();
-// const assistant = new Assistant();
-//azure.sample();
-//Delete All Assistants
-// await ai.deleteAssistants();
-//Create a new Assistant
-// var res = await(ai.createAssistant("company"));
-//初始化使用
-// var res = await ai.getAssistant("company");
-
-// //更新配置
-// ai.updateAssistant("company");
-// //创建Thread
-//  await ai.createThread();
-// //Sample
-// var msg = await ai.chat("未完了案件一覧を教えてください");
-// var msg = await ai.chat("FSR-0048");
-  //var msg = await ai.chat("稼働社員一覧は教えてください");
- //console.log("AIから：\r\n",msg);
-//   await ai.chat("社員数は何人ですか？");
-// var text = "未稼働社員は教えてください";
-// text = "未稼働社員一覧は教えてください";
-// text = "劉磊の説明文を教えてください";
-// var msg ="";
-//  //msg =  await ai.chat(text);
-//  text = "劉磊の説明文を任峰磊にメール送信します";
-//  msg =await ai.chat(text);
-// //会社の名前は
-//  text = "送信する";
-//  msg =await ai.chat(text);
-//  console.log("AIから：\r\n",msg);
-// 未稼働一覧は教えてください
-// 劉磊の説明文を教えてください。
-// 未稼働社員で劉磊がいますか？
-
-
-
-// import { Readable } from 'stream';
-
-// var path = "/Users/fengleiren/git/ainb/src/server/public/wav/1718536871971.wav";
-// var buffer = fs.readFileSync(path)
-// const stream = Readable.from(buffer);
-
-
-//   const transcription = await openai.audio.transcriptions.create({
-//     file: await toFile(buffer, "audio.wav", {
-//         contentType: "audio/wav",
-//       }),
-//     model: "whisper-1",
-//   });
-
-
-var args =  { query: 'active', condition: 'list' };
-
-function getEmp(args) {
-
-    var status = util.getArg(args, ["status","employment_status"], {
-        "未稼働": "inactive",
-        "inactive": "inactive",
-        "unassigned": "inactive",
-        "稼働していない": "inactive",
-        "working": "active",
-        "active": "active"
-        , "稼働中": "active"
+export default class TestUtil {
+    constructor() {
+        // 初始化类属性
     }
-    );
-    if (status != null) {
-        args.status = status;
-        return args;
+    // 使用对象来存储类引用
+    classRegistry = {
+
+    };
+    createInstance(className, ...args) {
+        if (classRegistry[className]) {
+            return new classRegistry[className](...args);
+        } else {
+            throw new Error(`Class ${className} not found.`);
+        }
     }
 
-    var name = util.getArg(args, ["name"], null, "name=");
-    if (name != null) {
-        args.name = name;
+    async testAwait(instance, methodName, ...args) {
+        console.log(`############开始测试方法: ${methodName}##########\r\n`);
+
+        // const instance = new className();
+        const result = await instance[methodName](...args);
+
+        console.log(`方法 ${methodName} 的返回值: \r\n${result}\r\n`);
+        console.log(`########结束测试方法: ${methodName}#########\r\n`);
+
+        return result;
     }
-    return args;
+    test(instance, methodName, ...args) {
+        console.log(`############开始测试方法: ${methodName}##########\r\n`);
+
+        // const instance = new className();
+        const result = instance[methodName](...args);
+
+        console.log(`方法 ${methodName} 的返回值: \r\n${result}\r\n`);
+        console.log(`########结束测试方法: ${methodName}#########\r\n`);
+
+        return result;
+    }
 }
-//TEST
-console.log(getEmp({ query: 'active', condition: 'list' }));
-console.log(getEmp({ query: 'aaa', condition: 'active' }));
-console.log(getEmp({ query: 'all', condition: 'inactive' }));
-console.log(getEmp({query: 'employees', condition: {status:'inactive'}}));
-console.log(getEmp({query: 'employees', condition: {employment_status:'inactive'}}));
-console.log(getEmp({condition: 'name=XXX ', query: {}}));
+
+
+
+
+
