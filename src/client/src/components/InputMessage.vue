402e2d63b73cf688b08eeccb846963b50b729af3
Ren <javaandnet@gmail.com>
2024-06-28 01:31:57 +0900

    [bak]

--- src/client/src/components/InputMessage.vue
+++ src/client/src/components/InputMessage.vue
@@ -82,7 +82,7 @@ export default {
       let message = this.message;
       //FOR TEST
       if (this.TEST == true) {
-        message = "#0#";
+        message = "#1#";
       }
       this.$emit("sendMsg", {
         message: message,
